8e27b8253e883f4f6c6e31e674790c1b54574ffa
kouki-ozawa <neoreo038@icloud.com>
2024-12-10 12:51:56 +0900

    ライブラリのインストールと画像の読み込み

--- ai-rest/api/main.py
+++ ai-rest/api/main.py
@@ -1,8 +1,23 @@
+#ライブラリ
 from fastapi import FastAPI
+import tensorflow as tf
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
+
     return {"message": "野獣先輩"}
+
+image_path = ""
+image_size = 100
+image = cv2.imread(image_path)
+
+if image is None:
+    print("画像を読み込めません")
+    exit()
+
